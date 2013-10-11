92f1a2a2d84a7ffbaf8188ff49a485f89996487a
Rydekull <rydekull@gmail.com>
2013-10-11 09:59:16 +0200

    Update wrapper.sh
    Fixing so it doesnt break with sshupdate-command

--- deps/freebsd/wrapper.sh
+++ deps/freebsd/wrapper.sh
@@ -14,10 +14,13 @@ case "$SSH_ORIGINAL_COMMAND" in
 	"available-updates")
 		$PKGTOOL upgrade -q -n
 		;;
-	"patch-system")
+	"patch-n-reboot")
+		$PKGTOOL upgrade -q -y
+		;;
+	"system-upgrade")
 		$SYSTEMTOOL fetch install
 		;;
-	"patch-system-n-reboot")
+	"system-upgrade-n-reboot")
 		$SYSTEMTOOL fetch install
 		wall "Warning: System will reboot in 1 min"
 		wall "Remember to run patch-system after reboot"
@@ -25,20 +28,22 @@ case "$SSH_ORIGINAL_COMMAND" in
 		init 6
 		fi
                 ;;	
-#	"reboot-if-needed")
-#		LAST_KERNEL=$(rpm -q --last kernel | sed 's/^kernel-\([a-z0-9._-]*\).*/\1/g' | head -1)
-#		CURRENT_KERNEL=$(uname -r)
-#		test $LAST_KERNEL = $CURRENT_KERNEL || init 6
-#		;;
-#	"check-if-reboot-req")
-#		LAST_KERNEL=$(rpm -q --last kernel | sed 's/^kernel-\([a-z0-9._-]*\).*/\1/g' | head -1)
-#		CURRENT_KERNEL=$(uname -r)
-#		if [ $LAST_KERNEL != $CURRENT_KERNEL ]; then 
-#			echo "Reboot required"
-#		else
-#			echo "Already running on latest installed kernel"
-#		fi
-#		;;
+	"reboot-if-needed")
+	        echo "Function not done yet" ; exit 1
+		LAST_KERNEL=$(echo)
+		CURRENT_KERNEL=$(echo)
+		test $LAST_KERNEL = $CURRENT_KERNEL || init 6
+		;;
+	"check-if-reboot-req")
+	        echo "Function not done yet" ; exit 1
+		LAST_KERNEL=$(echo)
+		CURRENT_KERNEL=$(echo)
+		if [ $LAST_KERNEL != $CURRENT_KERNEL ]; then 
+			echo "Reboot required"
+		else
+			echo "Already running on latest installed kernel"
+		fi
+		;;
 	*)
 		echo "Sorry. Only these commands are available to you:"
 		echo "inventory patch available-updates patch-system patch-system-n-reboot"
